dd448ad0d5f6b86ac44cdfb352038d435a5d4e11
NilsonKr <nuotrok444@gmail.com>
2021-05-25 17:29:33 -0500

    :sparkles: [ADD] Generate tags with compose functions

--- Project/js/main.js
+++ Project/js/main.js
@@ -24,6 +24,31 @@ document.body.addEventListener('keydown', ev => {
 	}
 });
 
+//Compose Functions
+
+const getAttr = (attrObj = {}) => {
+	const attributes = [];
+
+	for (const attr in attrObj) {
+		attributes.push(` ${attr}="${attrObj[attr]}" `);
+	}
+
+	return attributes.join(' ');
+};
+
+const genTag = tag => content =>
+	`<${tag.name} ${getAttr(tag.attrs)} >${content}</${tag.name}>`;
+
+console.log(
+	genTag({
+		name: 'h1',
+		attrs: {
+			class: 'mx-3 flx-h',
+			id: 'Hello',
+		},
+	})('Hello World!')
+);
+
 //Validation Handle
 
 function handleAdd() {
