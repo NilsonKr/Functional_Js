ddb760312a2e69102195c5e21ffc42b5a5aa83b6
NilsonKr <nuotrok444@gmail.com>
2021-05-25 04:17:23 -0500

    :memo: [ADD] Validation for inputs

--- Project/js/main.js
+++ Project/js/main.js
@@ -3,8 +3,39 @@ const compose =
 	data =>
 		functions.reduceRight((value, func) => func(value), data);
 
+//Nodes
 const descriptionInput = document.querySelector('#description');
 const caloriesInput = document.querySelector('#calories');
 const carbsInput = document.querySelector('#carbs');
 const proteinInput = document.querySelector('#proteins');
+const submitBtn = document.querySelector('#submit');
 
+//Events
+submitBtn.addEventListener('click', handleAdd);
+
+document.body.addEventListener('keydown', ev => {
+	const inputsId = ['description', 'calories', 'carbs', 'proteins'];
+	const target = ev.target;
+
+	if (inputsId.includes(target.id) && target.classList.contains('is-invalid')) {
+		target.classList.remove('is-invalid');
+	}
+});
+
+//Validation Handle
+
+function handleAdd() {
+	!descriptionInput.value ? descriptionInput.classList.add('is-invalid') : '';
+	!caloriesInput.value ? caloriesInput.classList.add('is-invalid') : '';
+	!carbsInput.value ? carbsInput.classList.add('is-invalid') : '';
+	!proteinInput.value ? proteinInput.classList.add('is-invalid') : '';
+
+	if (
+		descriptionInput.value &&
+		carbsInput.value &&
+		caloriesInput.value &&
+		proteinInput.value
+	) {
+		console.log('Ok!');
+	}
+}
